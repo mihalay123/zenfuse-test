fc58df81598c1bdd9125db7297847ab3a4785bf3
mihalay123 <mihalay26@gmail.com>
2022-01-08 21:48:46 +0300

    fix incorrect margin

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './output.css'
 
 function App() {
 	return (
-		<div className="w-[100vh] h-[100vh] bg-white dark:bg-[#171717]">
+		<div className="h-[100vh] bg-white dark:bg-[#171717]">
 			<div className="w-[338px] h-[76px] position: absolute inset-2/4 flex flex-row justify-between items-center dark:true-gray-900 bg-opacity-0">
 				<Switcher />
 				<SliderContainer />
